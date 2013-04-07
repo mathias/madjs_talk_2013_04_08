8d674a6caf2e7176e1142278a8e59ae8d8f31ed8
Matt Gauger <matt.gauger@gmail.com>
2013-04-07 11:52:29 -0500

    Slides 1 and 2:
    - Values and Expressions
    - Values and Identity

---
+++ slide_1.js
@@ -0,0 +1,10 @@
+/*
+ * 1. Values and Expressions
+ */
+
+// All values are expressions
+
+// Is this an expression or a value?
+42
+
+"Javascript" + " is made up of " + "expressions and values"
---
+++ slide_2.js
@@ -0,0 +1,11 @@
+/*
+ * 2. Values and Identity
+ */
+
+// The === operator tests whether two values are identical
+
+2 === 2
+
+'hello' !== 'goodbye'
+
+2 === '2'
